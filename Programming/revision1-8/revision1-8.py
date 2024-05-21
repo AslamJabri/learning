7a29e9c7881c75d11dccc11dc5b805aacccb98a1
aslamjabri <jabri.aslam@outlook.com>
2024-05-21 12:29:43 +0530

    Programming started again now consistent

---
+++ Programming/revision1-8/revision1-8.py
@@ -0,0 +1,4 @@
+print("Welcome to the Band Name Generator")
+city_name = input("What's the name of the city you grew up in?\n").title()
+pet_name = input("What's your pet's name?\n").title()
+print("Your band name could be ",city_name +" "+pet_name)
